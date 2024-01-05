7669cc3622d8be92ae9d2aaba5be758e6174f251
krystein <nworahchimaobic@gmail.com>
2024-01-05 16:24:35 +0100

    project

---
+++ index.js
@@ -0,0 +1,9 @@
+function toggleNav() {
+    var navLinks = document.getElementById("navLinks");
+    navLinks.classList.toggle("show");
+  }
+  
+  function closeNav() {
+    var navLinks = document.getElementById("navLinks");
+    navLinks.classList.remove("show");
+  }
