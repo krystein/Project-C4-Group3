85f3eb061732337b32559c6ce16f9c2fa71fd1a1
krystein <nworahchimaobic@gmail.com>
2024-01-06 14:58:00 +0100

    project

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 function toggleNav() {
-    var navLinks = document.getElementById("navLinks");
-    navLinks.classList.toggle("show");
-  }
-  
-  function closeNav() {
-    var navLinks = document.getElementById("navLinks");
-    navLinks.classList.remove("show");
-  }
+  var navLinks = document.getElementById("navLinks");
+  navLinks.classList.toggle("show");
+}
+
+function closeNav() {
+  var navLinks = document.getElementById("navLinks");
+  navLinks.classList.remove("show");
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+window.onscroll = showScrollUpButton;
+
+function showScrollUpButton() {
+  var scrollUpBtn = document.getElementById("scrollUpBtn");
+  scrollUpBtn.style.display = (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) ? "block" : "none";
+}
+
+function scrollToTop() {
+  document.documentElement.scrollIntoView({ behavior: 'smooth' });
+}
+
